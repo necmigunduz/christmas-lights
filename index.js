ab340ab57a73571f1791585109e00723bc1693ca
necmigunduz <necmigunduz@gmail.com>
2022-10-21 15:01:19 +0300

    Add light to the title

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ const circle = document.getElementsByClassName("circle");
 const play = document.getElementById("play");
 const stop = document.getElementById("stop");
 const submit = document.getElementById("submit");
+const title = document.getElementById("title");
+
+console.log(title);
 
 var len = circle.length;
 
 const on = function () {
+  title.removeAttribute("style");
+  title.style.animationPlayState = "running";
+  title.style.WebkitAnimationPlayState = "running";
   for (var i = 0; i < len; i++) {
     circle[i].removeAttribute("style");
     circle[i].style.animationPlayState = "running";
@@ -13,7 +19,7 @@ const on = function () {
   }
 };
 const off = function () {
-  // title.style.animation = "none";
+  title.style.animation = "none";
 
   for (var i = 0; i < len; i++) {
     circle[i].style.animation = "none";
@@ -23,10 +29,10 @@ const off = function () {
 
 const convert = function () {
   const speed = document.getElementById("quantity").value;
-  for(let i=0;i<len;i++){
-    circle[i].style.animationDuration = (11-speed) + 's';
+  for (let i = 0; i < len; i++) {
+    circle[i].style.animationDuration = 11 - speed + "s";
   }
 };
-submit.addEventListener("click", convert)
+submit.addEventListener("click", convert);
 play.addEventListener("click", on);
 stop.addEventListener("click", off);
